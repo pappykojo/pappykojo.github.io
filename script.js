d602993ccc14ae21417ce23ff8f216903d892d0f
Oteng "Pappy" Tano Kojo <otengtano123@gmail.com>
2024-07-09 10:29:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const repoOwner = 'pappykojo'; // Replace with your GitHub username
-    const repoName = 'pappykojo.github.io'; // Replace with your repository name
-    const branchName = 'main'; // Replace with your branch name
+    const repoOwner = 'pappykojo';
+    const repoName = 'pappykojo.github.io';
+    const branchName = 'main';
 
     const fileListElement = document.getElementById('fileList');
 
     fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents?ref=${branchName}`)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
         .then(data => {
             data.forEach(item => {
                 const listItem = document.createElement('li');
@@ -19,5 +24,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         })
         .catch(error => {
             console.error('Error fetching repository contents:', error);
+            fileListElement.textContent = 'Error fetching repository contents. Please try again later.';
         });
 });
